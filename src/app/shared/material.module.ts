7241f8e71918abc85cf39cac2a151e005e1fe7de
shivani <shivani.gajjar25@gmail.com>
2020-04-16 11:20:44 +0530

    add button from dashboard

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -5,6 +5,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import {MatNativeDateModule, MatRippleModule} from '@angular/material/core';
 
@@ -19,7 +20,8 @@ import {MatNativeDateModule, MatRippleModule} from '@angular/material/core';
     MatInputModule,
     MatDatepickerModule,
     MatNativeDateModule,
-    MatRippleModule
+    MatRippleModule,
+    MatButtonModule
     
   ],
   declarations: []
